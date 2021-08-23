114603a70c4c6f11180f9e72a5219922c42853a1
yuu341 <oz.g.341@gmail.com>
2021-08-23 14:37:47 +0900

    add frontend

---
+++ react/cheet/create_app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+npx create-react-app app --template redux-typescript
+cd app
+yarn add @material-ui/core
+yarn add react-router
+yarn add react-router-dom
+yarn add -D @types/react-router
+yarn add -D @types/react-router-dom
+
+yarn add @material-ui/core
+yarn add @material-ui/icons
+yarn add @material-ui/styles
+yarn add @material-ui/data-grid
